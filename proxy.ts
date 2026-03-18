532bf1203f0e785138a3f98de11f6a46e723d5ec
unknown <viralrarsh@gmail.com>
2026-03-18 10:45:51 +0100

    new change updated 2

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     const accessToken = request.cookies.get('accessToken')?.value
     if (!accessToken) {
@@ -13,4 +13,4 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: ['/dashboard/:path*'],
-} 
+}
